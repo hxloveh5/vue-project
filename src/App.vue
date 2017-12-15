c2148e943aafbdd63a6e9da5847c227c8e736fef
project <project@.git.com>
2017-12-15 09:35:58 +0800

    chang

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="app">
+    <div class="change" @click="handleClick">{{message}}</div>
     
-    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+  	return {
+  		message: 'hello world'
+  	}
+  },
+  methods: {
+  	handleClick () {
+  		this.message = 'bey world'
+  	}
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+	.change {
+		color: red;
+	}
 </style>
