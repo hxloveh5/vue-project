02fc4e4d337d9ef894f98d5e8dba83113bf2e184
xhh <505443206@qq.com>
2017-12-16 22:06:24 +0800

    20171216-lwxh-index

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router'
 import './assets/style/reset.css'
 import 'swiper/dist/css/swiper.css'
 import VueSwiper from 'vue-awesome-swiper'
+import './assets/style/iconfont/iconfont.css'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 Vue.use(VueSwiper)
+Vue.use(VueResource)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,31 +1,32 @@
 <template>
  <div>
   <header class="header">
- 	<div class="back">返回</div>
- 	<div class="search">输入城市/景点/游玩主题</div>
- 	<div class="city">北京</div>	
+ 	<div class="back iconfont">&#xe624;</div>
+ 	<div class="search iconfont">&#xe604;输入城市/景点/游玩主题</div>
+ 	<div class="city iconfont">北京&#xe601;</div>	
   </header>
-  <swiper :options="swiperOption" ref="mySwiper">
-    <swiper-slide>
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item in swiperInfo" :key="item.id">
      <div class="swiper-img-con">
-      <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1609/15/630b82d932a3c402.jpg_640x200_862e836b.jpg"/>
+      <img class="swiper-img" :src="item.imgUrl"/>
      </div>
-     
     </swiper-slide>
-    <swiper-slide>
-     <div class="swiper-img-con">
-      <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1711/cd/5ff8869f365e7e02.jpg_640x200_097b1798.jpg"/>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+
+  <swiper>
+    <swiper-slide v-for="(pageInfo, index) in pages" :key="index">
+     <div class="icon-wrapper">
+     <div v-for="item in pageInfo" :key="item.id" class="icon-item">
+       <div class="icon-img-con">
+        <img class="icon-img" :src="item.imgUrl"/>
+       </div>
       </div>
+     </div>
     </swiper-slide>
-    <swiper-slide></swiper-slide>
-    <swiper-slide></swiper-slide>
-    <swiper-slide></swiper-slide>
-    <swiper-slide></swiper-slide>
-    <swiper-slide></swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+   </swiper> 
  </div>
+ 
 </template>
 
 <script>
@@ -33,11 +34,45 @@ export default {
   name: 'Index',
   data () {
     return {
+      swiperInfo: [],
+      iconInfo: [],
       swiperOption: {
         autoplay: 1000,
-        direction: 'horizontal'
+        direction: 'horizontal',
+        pagination: '.swiper-pagination',
+        loop: true
       }
     }
+  },
+  computed: {
+    pages () {
+      var pageArr = []
+      this.iconInfo.forEach((value, index) => {
+        let page = Math.floor(index / 8)
+
+        if (!pageArr[page]) {
+          pageArr[page] = []
+        }
+        pageArr[page].push(value)
+      })
+      return pageArr
+    }
+  },
+  methods: {
+    getIndexData () {
+      this.$http.get('/static/index.json')
+       .then(this.handleGetDataSucc.bind(this))
+    },
+    handleGetDataSucc (res) {
+      const body = res.body
+      if (body && body.data && body.data.swiper) {
+        this.swiperInfo = body.data.swiper
+        this.iconInfo = body.data.icons
+      }
+    }
+  },
+  created () {
+    this.getIndexData()
   }
 }
 </script>
@@ -50,24 +85,47 @@ export default {
  }
  .back{
  	width:0.64rem;
- 	line-height:0.86rem; 
+ 	line-height:0.86rem;
+  text-align:center; 
  }
  .search{
  	flex:1;
  	margin:0.14rem 0.18rem;
  	background:#fff;
  	border-radius:0.1rem;
-
+  color:#eaeaea;
+  line-height:0.6rem;
  }
  .city{
  	width:1.14rem;
  	line-height:0.86rem;
  	text-align:center;
+  text-overflow:ellipsis;
+  word-wrap:nowrap;
  }
  .swiper-img-con{
 	width:100%;
  }
  .swiper-img{
  	width:100%;
+ }
+ .icon-wrapper{
+ 
+ }
+ .icon-item{
+  box-sizing:border-box;
+  width:25%;
+  float:left;
+  padding:0.4rem;
+ }
+.icon-img-con{
+  width:100%;
+  text-align:center;
+  height:0;
+  padding-bottom:100%;
+}
+ .icon-img{
+  width:100%;
+
  }
 </style>
