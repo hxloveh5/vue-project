ee74847ed093fc85080fb7511779b94382fe3896
xhh <505443206@qq.com>
2017-12-21 08:47:22 +0800

    详情页

---
+++ src/page/detail/detail.vue
@@ -0,0 +1,500 @@
+<template>
+ <div class="box">
+  <div class="scenic-spot">
+   <img class="scenic-spot-img" src="http://img1.qunarzz.com/p/tts7/1711/34/fb0d96a7e8b8ca02.jpg_640x360_c68e63ad.jpg">
+  	<div class="icon-background">
+     <span class="mp-icon-background"></span>
+  	 <span class="mp-icon iconfont">&#xe624;</span>	
+  	</div>
+    <div class="mp-img-infocon">
+     <span class="mp-text-infocon-left">出发地:北京</span>
+     <span class="mp-text-infocon-right">蓝森国旅</span>
+  	</div>
+  </div>
+  <div class="mp-header">
+  <span class="header-icon iconfont">&#xe624;</span>
+   <h1 class="header-title">
+       天安门故宫珍宝馆+八达岭+颐和园+恭王府联票无线耳麦人工讲解
+   </h1>
+  </div>
+  <div class="map-footer">
+   <div class="footer-left">
+    <p class=" iconfont">&#xe676;</p> 
+    <p >咨询</p>
+   </div>
+   <h2 class="footer-right">立即支付</h2>
+  </div>
+  <div class="mp-name">
+  	<h2 class="guide">天安门故宫珍宝馆+八达岭+颐和园+恭王府联票无线耳麦人工讲解</h2>
+  	<div class="mp-price">
+  	 <em>&yen</em>
+  	 <span class="price">27</span>
+  	 <span class="prices">起</span>
+  	 <span class="date">可订12月20日</span>
+  	 <span class="sold-out">已售:40</span>
+  	</div>
+  	<div class="map-tags">
+  	 <div class="mp-tag-item">
+  	  <span class="mp-tag-item-icon iconfont">&#xe640;</span>
+  	  <span class="mp-icon-text">条件退</span>
+  	 </div>
+  	 <div class="mp-tag-item">
+  	  <span class="mp-tag-item-icon iconfont">&#xe640;</span>
+  	  <span class="mp-icon-text">铁定成团</span>
+  	 </div>
+  	 <div class="mp-tag-item">
+  	  <span class="mp-tag-item-icon iconfont">&#xe640;</span>
+  	  <span class="mp-icon-text">无自费</span>
+  	 </div>
+  	 <div class="mp-tag-item">
+  	  <span class="mp-tag-item-icon iconfont">&#xe640;</span>
+  	  <span class="mp-icon-text">无购物</span>
+  	 </div>
+  	</div>
+  </div>
+   <div class="main">
+      <span class="main-score-num">5.0</span>
+      <span class="subdivision">分</span>
+      <span class="degree-of-satisfaction">非常满意</span>
+      <span class="commentaries iconfont">2条评论&#xe603;</span>
+    </div>
+   <div class="star-comments">
+    <div class="start-img">
+     <span class="start-icon-img iconfont">&#xe606;&#xe606;&#xe606;&#xe606;&#xe606;</span>
+    <span>k*4 2017-12-04</span>
+    </div>
+    <p class="comments">不错,挺靠谱的,服务也不错</p>
+   </div>
+   <div class="gen-introduction">
+     <div class="stroke-intro">行程介绍</div>
+     <div class="costs-that">费用说明</div>
+     <div class="instructions-use">使用说明</div>
+   </div>
+   <div class="traffic-dining">
+     <div class="traffic-con">
+       <div class="traffic-icon">
+      <span class="iconfont">&#xe664;</span>
+      <span class="traffic">交通</span>
+       <div class="">汽车返航</div>
+     </div>
+     <div class="dining-icon">
+       <span class="iconfont">&#xe609;</span>
+       <span class="dining">用餐</span>
+       <div class="">餐食自理</div>
+     </div>
+     </div>
+     <div class="highlights">
+       <h3 class="tour-highlights">行程亮点</h3>
+      </div>
+      <div class="explain">
+        <p class="explain-text">★故宫深度游，不一样的体验</p>
+        <p class="explain-text">★逛帝王之家，走皇家园林</p>
+        <p class="explain-text">★无线耳麦*无需扎堆*聆听历史</p>
+      </div>
+      <div class="travel-arrangements">
+       <h3 class="travel-title">行程安排</h3>
+      </div>
+      <div class="plane">
+        <div class="times ">
+         07:30<em class="iconfont">&#xe602;</em>
+         <div class="line"></div>
+        </div>
+        <div class="assembling-place">
+          <p>行程安排</p>
+          <p class="assembling-text">北京市东城区天安门广场(人民英雄纪念碑北侧)</p>
+        </div>
+      </div>
+       <div class="plane">
+        <div class="times-31 ">
+         07:31<em class="tower-icon iconfont">&#xe611;</em>
+         <div class="lineone"></div>
+        </div>
+        <div class="assembling-place">
+         <p class="headings">基础版[天安门广场+故宫讲解]</p>
+         <p class="assembling-text">【游览路线】
+天安门广场---故宫（午门--太和门--太和殿--中和殿--保和殿--珍宝馆--景仁宫--交泰殿--坤宁宫--御花园）---解散 ***游览时间约180分钟
+（或继续参加升级版行程A/B/C）
+</p>
+        </div>
+      </div>
+       <div class="plane">
+        <div class="times ">
+         12:00<em class="dining-con iconfont">&#xe609;</em>
+         <div class="linetwo"></div>
+        </div>
+        <div class="assembling-place">
+         <p class="headings">午餐自理</p>
+        </div>
+      </div>
+      <div class="plane">
+       <div class="times-31 ">
+        12:30<em class="tower-icon iconfont">&#xe611;</em>
+         <div class="linethree"></div>
+        </div>
+        <div class="assembling-place">
+         <p class="headings">升级A【故宫+八达岭】</p>
+         <p class="assembling-text">【游览路线】
+天安门广场---故宫（赠珍宝馆）---八达岭---鸟巢水立方---解散***游览时间总约为300分钟
+（或自由活动/可自行散团；八达岭长城往返缆车如需体验自理140元/人，八达岭长城往返滑如需体验自理100元/人）
+（或继续参加升级版行程A/B/C）
+</p>
+        </div>
+      </div>
+       <div class="plane">
+       <div class="times-31 ">
+         12:30<em class="tower-icon iconfont">&#xe611;</em>
+        <div class="linefour"></div>
+       </div>
+        <div class="assembling-place">
+         <p class="headings">升级B【故宫+颐和园】</p>
+         <p class="assembling-text">【游览路线】
+天安门广场---故宫---颐和园---鸟巢水立方---解散***游览时间总约为300分钟（或自由活动/可自行散；颐和园内慈禧水道如需体验自理120元/人）
+</p>
+        </div>
+      </div>
+        <div class="plane">
+       <div class="times-31 ">
+         12:30<em class="tower-icon iconfont">&#xe611;</em>
+       </div>
+        <div class="assembling-place">
+         <p class="headings">升级C【故宫+恭王府】</p>
+         <p class="assembling-text">【游览路线】
+天安门广场---故宫---途径北海---恭王府---解散***游览时间总约为300分钟（或自由活动/可自行散团；黄包车为北京骑行文化，如需体验费用自理）
+</p>
+        </div>
+      </div> 
+    </div>
+ </div>
+</template>
+<script>
+	
+</script>
+<style scoped>
+.box {
+ background: #f5f5f5;
+}
+.mp-header {
+ position: fixed;
+ top: 0;
+ left: 0;
+ right: 0; 
+ bottom: 0;
+ height: 1rem;
+ display:flex;
+ text-align: center;
+ background: #00bcd4;
+ z-index: 1;
+}
+.header-icon {
+ line-height: 1rem;
+ width: .8rem;
+ color: #fff;
+} 
+.header-title{ 
+ width: 70% ;
+ white-space:nowrap;
+ overflow: hidden;
+ text-overflow: ellipsis;
+ line-height: 1rem;
+ font-size: .32rem;
+ color: #fff;
+ padding-left: .4rem;
+  }
+.map-footer{
+ display: flex;
+ position:fixed;
+ height: 1rem;
+ left: 0;
+ right: 0; 
+ bottom: 0;
+ z-index: 1;
+ background: #ff9800;
+}
+.footer-left {
+ width: 1rem;
+ height: 1rem;
+ background: #fff;
+ padding-top: .2rem;
+ text-align: center;
+}
+.footer-right {
+ line-height: 1rem;
+ margin: 0 auto;
+ font-size: .4rem;
+ color: #fff;
+}   
+.scenic-spot {
+ overflow: hidden;
+ position: relative;
+ height: 4rem;
+}
+.scenic-spot-img {
+ width: 100%;
+ min-height: 4rem;
+}
+.mp-icon-background {
+ display: block;
+ position: absolute;
+ border-radius: 50%;
+ text-indent: 2rem;
+ left: .05rem;
+ top: .05rem;
+ width: .72rem;
+ height: .72rem;
+ background: #000;
+ opacity: .5; 
+}
+.mp-icon {
+ display: block;
+ position: absolute;
+ line-height:.72rem;
+ text-align: center;
+ color: #fff;
+ text-indent: .17rem;
+ left: .05rem;
+ top: .05rem;
+} 
+.mp-img-infocon {
+ overflow: hidden;
+ width: 100%;
+ position: absolute;
+ left: 0;
+ bottom: 0;
+ display: flex;
+ justify-content: space-between;
+}
+.mp-text-infocon-left {
+ overflow: hidden;
+ margin-bottom: .1rem;
+ color: #fff;
+ font-size: .24rem;
+ line-height: .34rem;
+ white-space: nowrap;
+ text-overflow: ellipsis;
+ margin-left: .1rem;
+}
+.mp-text-infocon-right {
+ overflow: hidden;
+ margin-bottom: .1rem;
+ color: #fff;
+ font-size: .24rem;
+ line-height: .34rem;
+ white-space: nowrap;
+ text-overflow: ellipsis;
+ margin-right:0.1rem;
+}
+.mp-name {
+ position: relative;
+ margin-top: -.1rem;
+ padding: .2rem .2rem 0 .2rem;
+ background: #fff;
+ border-radius: .1rem .1rem 0 0;
+}
+.guide {
+overflow: hidden;
+font-size: .36rem;
+line-height: .44rem;
+text-overflow: ellipsis;
+color: #212121;	
+}
+.mp-price {
+ overflow: hidden; 
+ margin: .16rem 0;
+ height: .48rem;
+}
+.mp-price em {
+ font-size: .24rem;
+ color:#ff8300;
+}
+.mp-price . price{
+ font-size: .48rem;
+ color: #ff8300;
+}
+.sold-out {
+ float: right;
+ height: .24rem;
+ margin-top: .16rem;
+}
+.map-tags {
+ display: flex;
+ padding-bottom: .2rem;
+}
+.mp-tag-item-icon {
+ margin-right: .04rem;
+ color: #00afc7;
+ font-size: .28rem;
+ vertical-align: -.02rem;
+}
+.mp-icon-text {
+ color: #616161;
+ font-size: .2rem;
+ margin-right: .1rem;
+}
+.main{
+ width: 100%;
+ height: .8rem;
+ line-height: .8rem;
+ padding: .0 .2rem 0 .2rem;
+ margin-top: .2rem;
+ background-color:#fff;
+
+}
+.main-score-num {
+ font-size: .36rem;
+ line-height: .48rem;
+ color: #ff8300;
+}
+.subdivision {
+ color: #ff8300;
+ font-size: .24rem;
+}
+.degree-of-satisfaction {
+ color: #ff8300;
+ font-size: .24rem; 
+}
+.star-comments {
+  align-items: center;
+  box-sizing: border-box;
+  padding:0 .2rem;
+  background: #fff;
+}
+.commentaries {
+ font-size: .24rem;
+ color: #616161;
+ margin-right: .2rem;
+ margin-left: 4rem;
+}
+.start-img {
+ display: flex;
+ justify-content: space-between;
+ line-height: 0.6rem; 
+ border-top: 1px solid #f5f5f5;
+}
+.comments {
+  line-height: 0.5rem; 
+}
+.start-icon-img {
+  color: orange;
+}
+.gen-introduction {
+ display: flex;
+ line-height: 0.8rem;
+ border-bottom: 1px solid #f5f5f5;
+ background: #fff;
+ margin-top: 0.2rem;
+ text-align: center;  
+}
+.stroke-intro {
+ flex: 1;
+ border-bottom: .04rem solid #00afc7;
+ color: #00afc7;
+ 
+}
+.costs-that {
+ flex: 1;
+}
+.instructions-use {
+ flex: 1;
+}
+.traffic-dining {
+ background: #fff;
+ box-sizing: border-box;
+ padding: 0.2rem 0.2rem;
+
+}
+.traffic-con {
+ background: #f5f5f5;
+ border-radius: 0.1rem;
+ color: #919191;
+}
+.traffic-icon { 
+ display: flex;
+ padding: 0.2rem 0 0 0.2rem;
+}
+.dining-icon {
+ display: flex;
+ padding: 0.2rem 0.2rem;
+}
+.traffic {
+ color: #212121;
+ margin-right: .2rem;
+}
+.dining {
+ color: #212121;
+ margin:0 .2rem 0 .08rem;
+}
+.highlights {
+ line-height: 1rem;
+ font-size: .38rem;
+ color: #616161;;
+}
+.explain{
+ border-bottom: 1px dashed #eee;
+ padding-bottom: .2rem;
+}
+.explain-text {
+ line-height: .5rem;
+ color: #616161;;
+}
+.travel-title {
+ line-height: 1rem;
+ font-size: .38rem;
+ color:#616161; 
+}
+.timer-icon {
+ color:#00afc7;
+ margin: 0 .2rem;
+}
+.assembling-place{
+ color:#00afc7;
+ margin-left: .3rem; 
+}
+
+.plane {
+  display: flex;
+}
+.times em{
+  color: #00afc7;
+  margin-left: .3rem;
+}
+.timer-icon::after {
+  content: "";
+  border-left:1px solid red;
+}
+.assembling-text {
+  line-height: .5rem;
+  color:#616161;
+}
+.line {
+ height:1rem;
+ border-left: 1px solid #eee;
+ margin: .1rem 0  .1rem  1.15rem;
+}
+.tower-icon {
+ margin-left: .3rem;
+ color: #00afc7; 
+}
+.headings {
+ color: #212121; 
+}
+.lineone {
+ height:2rem;
+ border-left: 1px solid #eee;
+ margin: .1rem 0  .1rem  1.15rem;
+}
+.linetwo {
+ height: .2rem;
+ border-left: 1px solid #eee;
+ margin: .1rem 0  .1rem  1.15rem;
+}
+.linethree {
+  height: 3rem;
+  border-left: 1px solid #eee;
+  margin: .1rem 0  .1rem  1.15rem;
+}
+.linefour {
+ height: 2rem;
+ border-left: 1px solid #eee;
+ margin: .1rem 0  .1rem  1.15rem;
+}
+</style>
--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,370 +1,338 @@
 <template>
-<div>	
+ <div class="map-page">
   <header class="header">
-    <div class="back iconfont">&#xe624;</div>
-    <div class="search"><a href="#" class="prompt">输入城市/景点/游玩主题</a></div>
-    <div class="city"><span class="position">乌鲁木器大</span></div>
+  <div class="back iconfont">&#xe624;</div>
+  <div class="search iconfont">&#xe604;输入城市/景点/游玩主题</div>
+  <div class="city iconfont"><span class="position">北京</span>&#xe601;</div> 
   </header>
-  <swiper :options="swiperOption">
+  <swiper :options="swiperOption" class="swipers">
     <swiper-slide v-for="item in swiperInfo" :key="item.id">
-      <div class="swiper-img-con">
-        <img class="swiper-img" :src="item.imgUrl" />
-      </div>
-      
+     <div class="swiper-img-con">
+      <img class="swiper-img" :src="item.imgUrl"/>
+     </div>
     </swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 
   <swiper :options="iconsOption">
     <swiper-slide v-for="(pageInfo, index) in pages" :key="index">
-      <div class="icon-wrapper">
-        <div v-for="item in pageInfo" :key="item.id" class="icon-item">
-          <div class="icon-img-con">
-          <img class="icon-img" :src="item.imgUrl" />
-          <div class="title" v-text="item.title"></div>
-          </div>
-        </div>
+     <div class="icon-wrapper">
+     <div v-for="item in pageInfo" :key="item.id" class="icon-item">
+       <div class="icon-img-con">
+         <img class="icon-img" :src="item.imgUrl"/>
+         <div class="title" v-text="item.title"></div>
+       </div>
       </div>
+     </div>
     </swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
-  
-    <ul class="towli">
-    <li class="lili"><span class="lione iconfont">&#xe652;</span>定位失败</li>
-    <li><span class="litow iconfont">&#xe654;</span>5折泡温泉</li>
+    <div class="swiper-pagination" slot="pagination"></div>
+   </swiper>
+   <ul class="map">
+     <li class="map-item iconfont">&#xe602;定位失败</li>
+     <li class="iconfont">&#xe613;五折泡温泉</li>
   </ul>
-  <ul class="ultow">
-    <li class="lione"></li>
-    <li class="litow"></li>
-  </ul>
-  <h1 class="biaoti">
-    热门推荐
-  </h1>
-  <div class="hot" v-for="item in hotInfo" :key="item.id">
-    <div class="hot-img"><img :src="item.imgUrl" /></div>
-    <ul class="hot-right">
-      <li class="lione" v-text="item.title"></li>
-      <li class="litow" v-text="item.describe"></li>
-      <li class="lithree"><span class="spanone">￥</span><span v-text="item.price" class="spantow"></span><span class="spanthree">起</span></li>
+  <ul class="active">
+    <li class="active-itemone"></li>
+    <li class="active-itemtwo"></li>
+  </ul> 
+  <div class="hot-cakes">
+    <h2 class="headline">热销推荐</h2>
+     <div class="hot" v-for="item in hotInfo" :key="item.id">
+     <div class="hot-img"><img :src="item.imgUrl" /></div>
+      <ul class="hot-right">
+       <li class="hot-one" v-text="item.title"></li>
+        <li class="hot-two" v-text="item.describe"></li>
+      <li class="hot-three"><span class="sprice">￥</span><span v-text="item.price" class="spantow"></span><span class="spanthree">起</span></li> 
     </ul>
   </div>
-  <h2 class="more"><a class="morea" href="#">查看多有商品</a></h2>
-  <h1 class="biaotitow">周末去哪</h1>
-  <div class="go" v-for="item in goInfo" :key="item.id">
-    <div class="go-img"><img :src="item.imgUrl" /></div>
-    <ul class="go-right">
-      <li class="lione" v-text="item.title"></li>
-      <li class="litow" v-text="item.describe"></li>
-    </ul>
   </div>
-  <div class="zi">
-    <span class="piao-price">票面价</span>是指通过景区指定窗口售卖的纸质门票上标注的价格
+  <div class="allproduct">
+    <h2 class="lookall">查看所有商品</h2>
+    <div class="weekend">
+      <h3 class="go">周末去哪儿</h3>
+       <div class="week-go" v-for="item in weekInfo" :key="item.id">
+       <div class="week-img"><img :src="item.imgUrl" /></div>
+       <ul class="go-to-week">
+         <li class="goone" v-text="item.title"></li>
+         <li class="gotwo" v-text="item.describe"></li>
+       </ul>
+    </div>
   </div>
-  <ul class="iconfour">
-    <li><span class="iconfont">&#xe655;</span>机票</li>
-    <li><span class="iconfont">&#xe6e3;</span>酒店</li>
-    <li><span class="iconfont">&#xe646;</span>公寓</li>
-    <li><span class="iconfont">&#xe66c;</span>更多</li>
-  </ul>
-  <ul class="bottom">
-    <li><a href="#" >登录</a></li>
-    <li><a href="#" >我的订单</a></li>
-    <li><a href="#" >最近浏览</a></li>
-    <li><a href="#" >关于我们</a></li>
-  </ul>
-</div>
+  </div>
+ </div>
 </template>
 
 <script>
-  export default {
-    name: 'Index',
-    data () {
-      return {
-        swiperInfo: [],
-        iconsInfo: [],
-        hotInfo: [],
-        goInfo: [],
-        swiperOption: {
-          autoplay: 10000,
-          pagination: '.swiper-pagination',
-          loop: true,
-          initialSlide: 1
-        },
-        iconsOption: {
-          pagination: '.swiper-pagination',
-          initialSlide: 0
-        }
-      }
-    },
-
-    computed: {
-      pages () {
-        const pages = []
-        this.iconsInfo.forEach((value, index) => {
-          let page = Math.floor(index / 8)
-          if (!pages[page]) {
-            pages[page] = []
-          }
-          pages[page].push(value)
-        })
-        return pages
-      }
-    },
-
-    methods: {
-      getIndexData () {
-        this.$http.get('/static/index.json')
-        .then(this.handleGetDataSucc.bind(this))
+export default {
+  name: 'Index',
+  data () {
+    return {
+      swiperInfo: [],
+      iconInfo: [],
+      hotInfo: [],
+      weekInfo: [],
+      swiperOption: {
+        autoplay: 100000,
+        pagination: '.swiper-pagination',
+        loop: true
       },
-      handleGetDataSucc (res) {
-        const body = res.body
-        if (body && body.data && body.data.swiper) {
-          this.swiperInfo = body.data.swiper
-          this.iconsInfo = body.data.icons
-          this.hotInfo = body.data.hot
-          this.goInfo = body.data.go
-        }
+      iconsOption: {
+        pagination: '.swiper-pagination'
       }
-    },
-    created () {
-      this.getIndexData()
     }
+  },
+  computed: {
+    pages () {
+      const pageArr = []
+      this.iconInfo.forEach((value, index) => {
+        let page = Math.floor(index / 8)
+
+        if (!pageArr[page]) {
+          pageArr[page] = []
+        }
+        pageArr[page].push(value)
+      })
+      return pageArr
+    }
+  },
+  methods: {
+    getIndexData () {
+      this.$http.get('/static/index.json')
+       .then(this.handleGetDataSucc.bind(this))
+    },
+    handleGetDataSucc (res) {
+      const body = res.body
+      if (body && body.data && body.data.swiper) {
+        this.swiperInfo = body.data.swiper
+        this.iconInfo = body.data.icons
+        this.hotInfo = body.data.hot
+        this.weekInfo = body.data.go
+      }
+    }
+  },
+  created () {
+    this.getIndexData()
   }
+}
 </script>
+
 <style scoped>
-  .header{
-  	display: flex;
-  	background: #05bad5;
-  	color: #fff;
-  }
-  .back{
-  	width: .64rem;
-  	line-height: .86rem;
-    text-align: center;
-  }
-  .search{
-  	flex: 1;
-  	margin: .14rem .18rem;
-  	background: #fff;
-  	border-radius: .1rem;
-    color:;
-  }
-  .prompt {
-    font-size: 0.26rem;
-    color: #ccc;
-    line-height: 0.6rem;
-  }
-  .city{
-  	width:1.32rem;
-  	line-height: .86rem;
-  	text-align: left;
-  }
-  .position{
-    width:0.85rem;
-    display:block;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  .city::after{
-    content:"";
-    display: block;
-    width: 0px;
-    height: 0px;
-    border: 0.1rem solid #fff;
-    border-color: white transparent transparent transparent;
-    position: absolute;
-    right: 0.28rem;
-    top: 0.36rem;
-  }
-  .swiper-img-con{
-    width:100%;
-    height: 0;
-    overflow: hidden;
-    padding-bottom: 31.25%;
-  }
-  .swiper-img{
-    width:100%;
-  }
-  .icon-wrapper{
-    height:3.8rem;
-  }
-  .icon-item {
-    box-sizing: border-box;
-    width:25%;
-    float:left;
-    padding:0rem .6rem;
-    margin-top:.4rem;
-  }
-  .icon-img-con{
-    display:flex;
-    flex-direction:column;
-    justify-content: center;
-    align-items: center;
-  }
-  .icon-img {
-    width:100%;
-  }
-  .title{
-    padding-top:.2rem;
-    white-space: nowrap;
-    font-size:.28rem;
-    text-align:center;
-    color:#1f1f1f;
-  }
-  
-  .towli{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    border-top:0.01rem solid #dfdfdf;
-    border-bottom:0.17rem solid #f5f5f5;
-  }
-  .towli li{
-    height:0.96rem;
-    width:3.74rem;
-    text-align:center;
-    line-height:0.96rem;
-    font-size:.28rem;
-    color:#1f1f1f;
-  }
-  .towli .lili{
-    border-right:0.01rem solid #dfdfdf;
-  }
-  .ultow{
-    display:flex;
-    flex-direction:row;
-    justify-content:space-around;
-  }
-  .ultow .lione{
-    height:1.39rem;
-    width:3.75rem;
-    background:url(http://img1.qunarzz.com/piao/fusion/1710/a2/e395615b16fb1302.png) center center no-repeat;
-    background-size:cover;
-    border-right:0.01rem solid #dfdfdf;
-  }
-  .ultow .litow{
-    height:1.39rem;
-    width:3.75rem;
-    background:url(http://img1.qunarzz.com/piao/fusion/1711/8a/4c62d1a89fc2d602.png) center center no-repeat;
-    background-size:cover;
-  }
-  .biaoti{
-    height:0.81rem;
-    line-height:0.81rem;
-    color:#1f1f1f;
-    background:#f5f5f5;
-  }
-  .hot{
-    display:flex;
-    flex-direction: row;
-    justify-content: space-around;
-    height:1.4rem;
-    padding-top:0.24rem;
-    padding-bottom:0.24rem;
-    border-bottom:0.01rem solid #dfdfdf;
-  }
-  .hot-img img{
+ .map-page{
+  width:100%;
+  height:100%;
+  background:#f5f5f5; 
+ }
+ .header{
+  display:flex;
+  background:#01bdd6;
+  color:#fff;
+ }
+ .back{
+  width:0.64rem;
+  line-height:0.86rem;
+  text-align:center; 
+ }
+ .search{
+  flex:1;
+  margin:0.14rem 0.18rem;
+  background:#fff;
+  border-radius:0.1rem;
+  color:#eaeaea;
+  line-height:0.6rem;
+ }
+ .city{
+  width:1.14rem;
+  line-height:0.86rem;
+  text-align:center;
+ }
+/* .position{
+ 
+  display:block;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  } */
+ .swiper-img-con{
+  width:100%;
+  background:#fff;
+/*   margin-top:1rem; */
+ }
+ .swipers{
+
+ }
+ .swiper-img{
+  width:100%;
+ }
+ .icon-wrapper{
+ /* height:3.8rem; */
+ }
+ .icon-item{
+  box-sizing:border-box;
+  width:25%;
+  float:left;
+  padding:0.3rem;
+  background:#fff;
+ }
+.icon-img-con{
+
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items: center;
+  width:100%;
+}
+.icon-img{
+ width:50%;
+}
+.title{
+  text-align:center;
+  white-space:nowrap;
+  margin-top:0.2rem;
+}
+.map{
+  display: flex;
+  flex-direction:row;
+  justify-content:space-around;
+  align-items:center;
+  background:#fff;
+  border-top:0.01rem solid #dfdfdf;
+}
+.map li{
+ text-align:center;
+ width:49%;
+ line-height:0.98rem;
+}
+.map-item{
+ border-right:0.01rem solid #dfdfdf;
+}
+.active{
+ display: flex;
+ flex-direction:row;
+ justify-content:space-around;
+ align-items:center;
+ margin-top:0.2rem;
+ background:#fff;
+}
+.active-itemone{
+ width:3.75rem;
+ height:1.39rem; 
+ background:url(http://img1.qunarzz.com/piao/fusion/1710/a2/e395615b16fb1302.png)center center no-repeat;
+ background-size:cover;
+ border-right:0.01rem solid #dfdfdf;
+}
+.active-itemtwo{
+ width:3.75rem;
+ height:1.39rem; 
+ background:url(http://img1.qunarzz.com/piao/fusion/1711/8a/4c62d1a89fc2d602.png)center center no-repeat;
+ background-size:cover;
+}
+.headline{
+  height:0.81rem;
+  line-height: 0.81rem;
+  margin-left: 0.3rem;
+  color:#1f1f1f;
+  background:#f5f5f5;
+}
+.hot{
+  display:flex;
+  flex-direction:row;
+  height:1.4rem;
+  padding-top:0.24rem;
+  padding:0.24rem;
+  border-bottom:0.01rem solid #dfdfdf;
+  background:#fff; 
+}
+.hot-img img{
     width:1.4rem;
     height:1.4rem;
+}
+.hot-right{
+   width:80%; 
+}
+.hot-one{
+  width:50%; 
+  overflow: hidden;
+  margin-top: .04rem;
+  margin-bottom: .1rem;
+  margin-left:0.2rem;
+  color: #212121;
+  font-size: .3rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+ .hot-two{
+  /* width:80%; */ 
+  overflow: hidden;
+  margin-top: .04rem;
+  margin-bottom: .1rem;
+  margin-left:0.2rem;
+  color: #212121;
+  font-size: .3rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
   }
-  .hot .lione{
-    color:#1f1f1f;
-    font-size:.32rem;
-  }
-  .hot .litow{
+   .hot-three{
     padding-top:.2rem;
-    color:#9d9d9d;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
+    margin-left:0.16rem;
   }
-  .hot .lithree{
-    padding-top:.2rem;
-  }
-  .hot .lithree .spanone{
+   .hot-three .sprice{
     font-size: .2rem;
     color:#cfa072;
   }
-  .hot .lithree .spantow{
+  .hot-three .spantow{
     font-size: .4rem;
     color:#cfa072;
-
   }
-  .hot .lithree .spanthree{
+ /*.hot-three .spanthree{
     font-size: .24rem;
     color:#9d9d9d;
-  }
-  .more{
+  } */
+  .lookall{
     height: .88rem;
-    width:100%;
-    text-align:center;
+    color: #00afc7;
     line-height: .88rem;
+    text-align: center;
+    background:#fff;
   }
-  .biaotitow{
-    height:0.81rem;
-    line-height:0.81rem;
-    color:#1f1f1f;
-    background:#f5f5f5;
+ .go{
+  height:0.81rem;
+  line-height: 0.81rem;
+  margin-left: 0.3rem;
+  color:#1f1f1f;
+  background:#f5f5f5;
   }
-  .go{
-    display:flex;
-    flex-direction: column;
-    justify-content: space-around;
-    height:4.04rem;
-    margin-bottom:.15rem;
+.weekgo{
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height:4.04rem;
+  margin-bottom:.15rem;
+}
+.weekimg{
+  width:100%;
+  overflow: hidden;
+  height: 0.37rem;
+  padding-bottom: 37.4375%;
   }
-  .go-img{
-    height: 0;
-    overflow: hidden;
-    padding-bottom: 37.4375%;;
-  }
-  .go-img img{
-    width:100%;
-  }
-  .go-right{
-    height:.62rem;
-    padding:.08rem;
-  }
-  .go-right .lione{
-    color:#1f1f1f;
-    font-size:.28rem;
-  }
-  .go-right .litow{
-    padding-top:.2rem;
-    color:#9d9d9d;
-    font-size: .24rem;
-    padding-bottom:.4rem;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  .zi{
-    height:.3rem;
-    font-size:.24rem;
-    color: #616161;
-    border-top:0.10rem solid #f5f5f5;
-  }
-  .iconfour{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-  .iconfour li{
-    width:1.5rem;
-    height:.62rem;
-    text-align:center;
-    line-height:.62rem;
-    color: #9e9e9e;
-  }
-  .bottom{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-  .bottom li{
-    width:1.12rem;
-    height:.39rem;
-    font-size:.12rem;
-  }
+.week-img img{
+  width:100%;
+}
+.goone{
+  overflow: hidden;
+  padding-right: 1.4rem;
+  color: #212121;
+  font-size: .28rem;
+  line-height: .48rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.gotwo{
+  overflow: hidden;
+  padding-right: 1.4rem;
+  color: #212121;
+  font-size: .24rem;
+  line-height: .42rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
 </style>
