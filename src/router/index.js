615fcf23c0f79572d0cec952350e01a283e098f8
kathy <kathy57@qq.com>
2017-12-21 08:46:26 +0800

    wyorder

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/page/index/index'
+import Order from '@/page/order/order'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order
     }
   ]
 })
